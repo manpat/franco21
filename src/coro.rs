6820ab5da06d1bc7117c9841322b38c8d4e4d5d9
manpat <pat@patsown.com>
2018-01-23 19:14:41 +0000

    Added MutRc and Coro

---
+++ src/coro.rs
@@ -0,0 +1,99 @@
+use std::ops::{Generator, GeneratorState};
+
+pub struct Coro<Y> {
+	pub value: Option<Y>,
+	
+	valid: bool,
+	coro: Box<Generator<Yield=Y, Return=()>>,
+}
+
+impl<Y> Coro<Y> {
+	pub fn is_valid(&self) -> bool { self.valid }
+}
+
+impl<Y, G> From<G> for Coro<Y> where G: 'static + Generator<Yield=Y, Return=()> {
+	fn from(gen: G) -> Self {
+		Coro {
+			coro: box gen,
+			value: None,
+			valid: true,
+		}
+	}
+}
+
+impl<Y> Iterator for Coro<Y> {
+	type Item = Y;
+	default fn next(&mut self) -> Option<Self::Item> {
+		if !self.valid { return None }
+
+		if let GeneratorState::Yielded(yielded_value) = self.coro.resume() {
+			Some(yielded_value)
+		} else {
+			self.valid = false;
+			None
+		}
+	}
+}
+
+impl<Y: Clone> Iterator for Coro<Y> {
+	fn next(&mut self) -> Option<Self::Item> {
+		if !self.valid { return None }
+
+		if let GeneratorState::Yielded(yielded_value) = self.coro.resume() {
+			self.value = Some(yielded_value);
+			self.value.clone()
+		} else {
+			self.valid = false;
+			None
+		}
+	}
+}
+
+pub struct StackCoro<Y, G: Generator<Yield=Y, Return=()>> {
+	pub value: Option<Y>,
+
+	valid: bool,
+	coro: G,
+}
+
+impl<Y, G> StackCoro<Y, G> where G: Generator<Yield=Y, Return=()> {
+	pub fn is_valid(&self) -> bool { self.valid }
+}
+
+impl<Y, G> From<G> for StackCoro<Y, G> where G: Generator<Yield=Y, Return=()> {
+	fn from(gen: G) -> Self {
+		StackCoro {
+			coro: gen,
+			value: None,
+			valid: true,
+		}
+	}
+}
+
+impl<Y, G> Iterator for StackCoro<Y, G> where G: Generator<Yield=Y, Return=()> {
+	type Item = Y;
+	default fn next(&mut self) -> Option<Self::Item> {
+		if !self.valid { return None }
+
+		if let GeneratorState::Yielded(yielded_value) = self.coro.resume() {
+			Some(yielded_value)
+		} else {
+			self.valid = false;
+			None
+		}
+	}
+}
+
+impl<Y, G> Iterator for StackCoro<Y, G> where Y: Clone, G: Generator<Yield=Y, Return=()> {
+	fn next(&mut self) -> Option<Self::Item> {
+		if !self.valid { return None }
+
+		if let GeneratorState::Yielded(yielded_value) = self.coro.resume() {
+			self.value = Some(yielded_value);
+			self.value.clone()
+		} else {
+			self.valid = false;
+			None
+		}
+	}
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,22 @@
 #![feature(const_fn)]
+#![feature(box_syntax)]
 #![feature(ord_max_min)]
 #![feature(slice_patterns)]
+#![feature(specialization)]
 #![feature(inclusive_range_syntax)]
+#![feature(generators, generator_trait)]
 
 pub extern crate rand;
 
+pub mod mut_rc; 
 pub mod easing; 
-pub mod math; 
+pub mod math;
+pub mod coro;
 
+pub use mut_rc::*;
 pub use easing::*;
 pub use math::*;
+pub use coro::*;
 
 #[macro_export]
 macro_rules! match_pattern {
