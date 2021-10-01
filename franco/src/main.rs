2dd65e4da7d7da9f2215c3ef46a1689d49727f3a
Patrick Monaghan <0x5f.manpat@gmail.com>
2021-10-01 17:08:27 +0100

    window + prelude

--- franco/src/main.rs
+++ franco/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+pub mod prelude;
+use prelude::*;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut engine = toybox::Engine::new("franco21")?;
+
+    loop {
+        engine.process_events();
+        if engine.should_quit() {
+            break
+        }
+
+        engine.end_frame();
+    }
+
+    Ok(())
 }
+
+
---
+++ franco/src/prelude.rs
@@ -0,0 +1,1 @@
+pub use toybox::prelude::*;
